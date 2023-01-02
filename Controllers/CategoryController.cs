555efe60a039b563e684037e172bca5d02211f81
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-01-02 19:00:21 +0545

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDLearning.Models;
+using CRUDLearning.Data;
+using System.Collections.Generic;
+
+namespace CRUDLearning.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db= db;
+        }
+        
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objcategoriesList = _db.Categories;
+            return View(objcategoriesList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDB = _db.Categories.Find(id);
+            if(CategoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDB);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDB = _db.Categories.Find(id);
+            if (CategoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDB);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDLearning.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDLearning.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
